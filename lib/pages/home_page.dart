8e47341578a3d66d3df86532c901c4cd29e7c1fb
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-05-30 19:07:25 +0300

    home screen

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 48),
+
+              // Welcoming message
+              const Padding(padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: Text("Welcome"),
+              ),
+
+              // Start ordering msg
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: Text(
+                    "Start ordering your grocery",  // ADD FONT
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 24),
+
+              // NEW DIVIDER
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: Divider(),
+              ),
+
+              //fresh items + grid
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: Text(
+                  "Fresh items",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+
+              Expanded(child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                return GroceryItemTile();
+                },
+              ))
+
+
+            ],
+          )),
+    );
+  }
+}
