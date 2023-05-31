83d69b3e59e34c5f8d0c8dc0be5e273ea185bfa5
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-05-31 23:35:56 +0300

    grocery list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:groceryapp/components/grocery_item_tile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,54 +9,59 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 48),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 48),
 
-              // Welcoming message
-              const Padding(padding: EdgeInsets.symmetric(horizontal: 24.0),
-                child: Text("Welcome"),
+          // Welcoming message
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text("Welcome"),
+          ),
+
+          // Start ordering msg
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text(
+              "Start ordering your grocery", // ADD FONT
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
               ),
+            ),
+          ),
+          const SizedBox(height: 24),
 
-              // Start ordering msg
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24.0),
-                child: Text(
-                    "Start ordering your grocery",  // ADD FONT
-                  style: TextStyle(
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 24),
+          // NEW DIVIDER
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Divider(),
+          ),
 
-              // NEW DIVIDER
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24.0),
-                child: Divider(),
-              ),
+          //fresh items + grid
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text(
+              "Fresh items",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          // line divider
+          const Divider(
+            thickness: 4,
+            color: Colors.black,
+          ),
 
-              //fresh items + grid
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24.0),
-                child: Text(
-                  "Fresh items",
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-
-              Expanded(child: GridView.builder(
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          Expanded(
+              child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2),
-                itemBuilder: (context, index) {
-                return GroceryItemTile();
-                },
-              ))
-
-
-            ],
-          )),
+            itemBuilder: (context, index) {
+              return GroceryItemTile();
+            },
+          ))
+        ],
+      )),
     );
   }
 }
