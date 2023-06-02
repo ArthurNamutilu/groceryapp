84d2d9a48eaa402bda8c4aa2e903a74119a75f27
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-06-02 20:06:10 +0300

    works on linux but android

--- lib/model/cart_model.dart
+++ lib/model/cart_model.dart
@@ -1,1 +1,58 @@
+import 'package:flutter/material.dart';
 
+class CartModel extends ChangeNotifier {
+  // list of items on sale
+  final List _availableItems = [
+    // name price image color
+    ["Avocado", "50", "/lib/images/avocado.png", Colors.green],
+    ["Banana", "20", "/lib/images/banana.png", Colors.yellow],
+    ["Chicken", "650", "/lib/images/chicken.png", Colors.brown],
+    ["Water", "150", "/lib/images/water.png", Colors.blue],
+  ];
+
+  // get  shop item to return list
+  get shopItems => _availableItems;
+}
+
+/*  BORDER BORDER BORDER BORDER BORDER BORDER BORDER */
+
+// import 'package:flutter/material.dart';
+
+// class CartModel extends ChangeNotifier {
+//   // list of items on sale
+//   final List _shopItems = const [
+//     // [ itemName, itemPrice, imagePath, color ]
+//     ["Avocado", "4.00", "lib/images/avocado.png", Colors.green],
+//     ["Banana", "2.50", "lib/images/banana.png", Colors.yellow],
+//     ["Chicken", "12.80", "lib/images/chicken.png", Colors.brown],
+//     ["Water", "1.00", "lib/images/water.png", Colors.blue],
+//   ];
+
+//   // list of cart items
+//   List _cartItems = [];
+
+//   get cartItems => _cartItems;
+
+//   get shopItems => _shopItems;
+
+//   // add item to cart
+//   void addItemToCart(int index) {
+//     _cartItems.add(_shopItems[index]);
+//     notifyListeners();
+//   }
+
+//   // remove item from cart
+//   void removeItemFromCart(int index) {
+//     _cartItems.removeAt(index);
+//     notifyListeners();
+//   }
+
+//   // calculate total price
+//   String calculateTotal() {
+//     double totalPrice = 0;
+//     for (int i = 0; i < cartItems.length; i++) {
+//       totalPrice += double.parse(cartItems[i][1]);
+//     }
+//     return totalPrice.toStringAsFixed(2);
+//   }
+// }
--- lib/pages/intro_screen.dart
+++ lib/pages/intro_screen.dart
@@ -13,7 +13,7 @@ class IntroPage extends StatelessWidget {
           //logo
           Padding(
             padding: const EdgeInsets.only(
-              left: 80.0, right: 80, bottom: 40, top: 160),
+                left: 80.0, right: 80, bottom: 40, top: 160),
             child: Image.asset('lib/images/banana.png'),
           ),
           const Padding(
@@ -21,25 +21,24 @@ class IntroPage extends StatelessWidget {
             child: Text(
               'Welcome to your favorite grocery store',
               textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold
-              ),
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
             ),
           ),
-          const SizedBox(height: 24,),
-          //intro 
+          const SizedBox(
+            height: 24,
+          ),
+          //intro
           Text(
             "Fresh grocery daily",
-            style: TextStyle(color: Colors.green[600],
-              fontWeight: FontWeight.bold
-            ),
+            style: TextStyle(
+                color: Colors.green[600], fontWeight: FontWeight.bold),
           ),
           const Spacer(),
 
           GestureDetector(
-            onTap: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context){
-              return const HomePage();
+            onTap: () => Navigator.pushReplacement(context, MaterialPageRoute(
+              builder: (context) {
+                return const HomePage();
               },
             )),
             child: Container(
@@ -60,3 +59,90 @@ class IntroPage extends StatelessWidget {
     );
   }
 }
+
+/*  BORDER BORDER BORDER BORDER BORDER BORDER BORDER */
+
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'home_page.dart';
+
+// class IntroScreen extends StatelessWidget {
+//   const IntroScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.grey[50],
+//       body: SafeArea(
+//         child: Column(
+//           children: [
+//             // big logo
+//             Padding(
+//               padding: const EdgeInsets.only(
+//                 left: 100.0,
+//                 right: 100.0,
+//                 top: 120,
+//                 bottom: 20,
+//               ),
+//               child: Image.asset('lib/images/avocado.png'),
+//             ),
+
+//             // we deliver groceries at your doorstep
+//             Padding(
+//               padding: const EdgeInsets.all(28.0),
+//               child: Text(
+//                 'We deliver groceries at your doorstep',
+//                 textAlign: TextAlign.center,
+//                 style: GoogleFonts.notoSerif(
+//                     fontSize: 36, fontWeight: FontWeight.bold),
+//               ),
+//             ),
+
+//             // groceree gives you fresh vegetables and fruits
+//             Text(
+//               'Fresh items everyday',
+//               textAlign: TextAlign.center,
+//               style: TextStyle(
+//                 fontSize: 16,
+//                 color: Colors.grey[700],
+//               ),
+//             ),
+
+//             const SizedBox(height: 24),
+
+//             const Spacer(),
+
+//             // get started button
+//             GestureDetector(
+//               onTap: () => Navigator.pushReplacement(
+//                 context,
+//                 MaterialPageRoute(
+//                   builder: (context) {
+//                     return HomePage();
+//                   },
+//                 ),
+//               ),
+//               child: Container(
+//                 padding: const EdgeInsets.all(24),
+//                 decoration: BoxDecoration(
+//                   borderRadius: BorderRadius.circular(16),
+//                   color: const Color.fromARGB(255, 112, 91, 222),
+//                 ),
+//                 child: const Text(
+//                   "Get Started",
+//                   style: TextStyle(
+//                     color: Colors.white,
+//                     // fontWeight: FontWeight.bold,
+//                     fontSize: 16,
+//                   ),
+//                 ),
+//               ),
+//             ),
+
+//             const Spacer(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
