213eb8c238ea55819bce9f9b05c7fed5fc9c82ee
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-06-03 23:29:34 +0300

    working model

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,49 @@
-import 'package:flutter/material.dart';
-import 'package:groceryapp/model/cart_model.dart';
-import 'package:provider/provider.dart';
-import 'pages/intro_screen.dart';
-
-void main() => runApp(
-      const MyApp(),
-    );
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // wrapping entire app with change notifier provider and give it cart model & child give it our app
-    return ChangeNotifierProvider(
-        create: (context) => CartModel(),
-        child: const MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: IntroPage(),
-        ));
-  }
-}
-
-/*  BORDER BORDER BORDER BORDER BORDER BORDER BORDER */
 // import 'package:flutter/material.dart';
 // import 'package:groceryapp/model/cart_model.dart';
 // import 'package:provider/provider.dart';
 // import 'pages/intro_screen.dart';
 
-// void main() {
-//   runApp(const MyApp());
-// }
+// void main() => runApp(
+//       const MyApp(),
+//     );
 
 // class MyApp extends StatelessWidget {
 //   const MyApp({super.key});
 
 //   @override
 //   Widget build(BuildContext context) {
+//     // wrapping entire app with change notifier provider and give it cart model & child give it our app
 //     return ChangeNotifierProvider(
-//       create: (context) => CartModel(),
-//       child: const MaterialApp(
-//         debugShowCheckedModeBanner: false,
-//         home: IntroScreen(),
-//       ),
-//     );
+//         create: (context) => CartModel(),
+//         child: const MaterialApp(
+//           debugShowCheckedModeBanner: false,
+//           home: IntroPage(),
+//         ));
 //   }
 // }
+
+/*  BORDER BORDER BORDER BORDER BORDER BORDER BORDER */
+import 'package:flutter/material.dart';
+import 'package:groceryapp/model/cart_model.dart';
+import 'package:provider/provider.dart';
+import 'pages/intro_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // using imported provider
+    return ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: IntroScreen(),
+      ),
+    );
+  }
+}
