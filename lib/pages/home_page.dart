8aba7e074d066eb190d7f36e17dae59472f18e90
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-06-03 23:32:30 +0300

    home with irem list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,229 +1,230 @@
-import 'package:flutter/material.dart';
-import 'package:groceryapp/components/grocery_item_tile.dart';
-import 'package:groceryapp/model/cart_model.dart';
-import 'package:provider/provider.dart';
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 48),
-
-          // Welcoming message
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Text("Welcome"),
-          ),
-
-          // Start ordering msg
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Text(
-              "Start ordering your grocery", // ADD FONT
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          const SizedBox(height: 24),
-
-          // NEW DIVIDER
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Divider(),
-          ),
-
-          //fresh items + grid
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Text(
-              "Fresh items",
-              style: TextStyle(fontSize: 16),
-            ),
-          ),
-          // line divider
-          const Divider(
-            thickness: 4,
-            color: Colors.black,
-          ),
-
-          Expanded(
-            // wrapping the grid in consumer
-            child: Consumer<CartModel>(
-              builder: (context, value, child) {
-                return GridView.builder(
-                  itemCount: value.shopItems.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2),
-                  itemBuilder: (context, index) {
-                    // create module for cart to use as parameter for the GroceryItemTile() in cart-model.dart file
-                    //   return GroceryItemTile(
-                    //     itemName: value.shopItems[index][0],
-                    //     itemPrice: value.shopItems[index][1],
-                    //     imagePath: value.shopItems[index][2],
-                    //     color: value.shopItems[index][3],
-                    //   );
-                    //
-                  },
-                );
-              },
-            ),
-          )
-        ],
-      )),
-    );
-  }
-}
-
-/*  BORDER BORDER BORDER BORDER BORDER BORDER BORDER */
-
 // import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
+// import 'package:groceryapp/components/grocery_item_tile.dart';
+// import 'package:groceryapp/model/cart_model.dart';
 // import 'package:provider/provider.dart';
-// import '../components/grocery_item_tile.dart';
-// import '../model/cart_model.dart';
-// import 'cart_page.dart';
 
-// class HomePage extends StatefulWidget {
-//   const HomePage({super.key});
+// class HomePage extends StatelessWidget {
+//   const HomePage({Key? key}) : super(key: key);
 
-//   @override
-//   State<HomePage> createState() => _HomePageState();
-// }
-
-// class _HomePageState extends State<HomePage> {
 //   @override
 //   Widget build(BuildContext context) {
 //     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.transparent,
-//         elevation: 0,
-//         leading: Padding(
-//           padding: const EdgeInsets.only(left: 24.0),
-//           child: Icon(
-//             Icons.location_on,
-//             color: Colors.grey[700],
-//           ),
-//         ),
-//         title: Text(
-//           'Sydney, Australia',
-//           style: TextStyle(
-//             fontSize: 16,
-//             color: Colors.grey[700],
-//           ),
-//         ),
-//         centerTitle: false,
-//         actions: [
-//           Padding(
-//             padding: const EdgeInsets.only(right: 24.0),
-//             child: Container(
-//               padding: EdgeInsets.all(16),
-//               decoration: BoxDecoration(
-//                 color: Colors.grey[200],
-//                 borderRadius: BorderRadius.circular(12),
-//               ),
-//               child: Icon(
-//                 Icons.person,
-//                 color: Colors.grey,
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         backgroundColor: Colors.black,
-//         onPressed: () => Navigator.push(
-//           context,
-//           MaterialPageRoute(
-//             builder: (context) {
-//               return CartPage();
-//             },
-//           ),
-//         ),
-//         child: const Icon(Icons.shopping_bag),
-//       ),
-//       body: Column(
+//       body: SafeArea(
+//           child: Column(
 //         crossAxisAlignment: CrossAxisAlignment.start,
 //         children: [
 //           const SizedBox(height: 48),
 
-//           // good morning bro
+//           // Welcoming message
 //           const Padding(
 //             padding: EdgeInsets.symmetric(horizontal: 24.0),
-//             child: Text('Good morning,'),
+//             child: Text("Welcome"),
 //           ),
 
-//           const SizedBox(height: 4),
-
-//           // Let's order fresh items for you
-//           Padding(
-//             padding: const EdgeInsets.symmetric(horizontal: 24.0),
+//           // Start ordering msg
+//           const Padding(
+//             padding: EdgeInsets.symmetric(horizontal: 24.0),
 //             child: Text(
-//               "Let's order fresh items for you",
-//               style: GoogleFonts.notoSerif(
+//               "Start ordering your grocery", // ADD FONT
+//               style: TextStyle(
 //                 fontSize: 36,
 //                 fontWeight: FontWeight.bold,
 //               ),
 //             ),
 //           ),
-
 //           const SizedBox(height: 24),
 
+//           // NEW DIVIDER
 //           const Padding(
 //             padding: EdgeInsets.symmetric(horizontal: 24.0),
 //             child: Divider(),
 //           ),
 
-//           const SizedBox(height: 24),
-
-//           // categories -> horizontal listview
-//           Padding(
-//             padding: const EdgeInsets.symmetric(horizontal: 24.0),
+//           //fresh items + grid
+//           const Padding(
+//             padding: EdgeInsets.symmetric(horizontal: 24.0),
 //             child: Text(
-//               "Fresh Items",
-//               style: GoogleFonts.notoSerif(
-//                 //fontWeight: FontWeight.bold,
-//                 fontSize: 18,
-//               ),
+//               "Fresh items",
+//               style: TextStyle(fontSize: 16),
 //             ),
 //           ),
+//           // line divider
+//           const Divider(
+//             thickness: 4,
+//             color: Colors.black,
+//           ),
 
-//           // recent orders -> show last 3
 //           Expanded(
+//             // wrapping the grid in consumer
 //             child: Consumer<CartModel>(
 //               builder: (context, value, child) {
 //                 return GridView.builder(
-//                   padding: const EdgeInsets.all(12),
-//                   physics: const NeverScrollableScrollPhysics(),
 //                   itemCount: value.shopItems.length,
 //                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-//                     crossAxisCount: 2,
-//                     childAspectRatio: 1 / 1.2,
-//                   ),
+//                       crossAxisCount: 2),
 //                   itemBuilder: (context, index) {
+//                     //create module for cart to use as parameter for the GroceryItemTile() in cart-model.dart file
 //                     return GroceryItemTile(
 //                       itemName: value.shopItems[index][0],
 //                       itemPrice: value.shopItems[index][1],
 //                       imagePath: value.shopItems[index][2],
 //                       color: value.shopItems[index][3],
-//                       onPressed: () =>
-//                           Provider.of<CartModel>(context, listen: false)
-//                               .addItemToCart(index),
 //                     );
 //                   },
 //                 );
 //               },
 //             ),
-//           ),
+//           )
 //         ],
-//       ),
+//       )),
 //     );
 //   }
 // }
+
+/*  BORDER BORDER BORDER BORDER BORDER BORDER BORDER */
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import '../components/grocery_item_tile.dart';
+import '../model/cart_model.dart';
+import 'cart_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        // leading: Padding(
+        //   padding: const EdgeInsets.only(left: 24.0),
+        //   child: Icon(
+        //     Icons.location_on,
+        //     color: Colors.grey[700],
+        //   ),
+        // ),
+        title: Text(
+          'Man from Kitale',
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey[700],
+          ),
+        ),
+        centerTitle: false,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Icon(
+                Icons.person,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return CartPage();
+            },
+          ),
+        ),
+        child: const Icon(Icons.shopping_bag),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 48),
+
+          // good morning bro
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text('Good morning,'),
+          ),
+
+          const SizedBox(height: 4),
+
+          // Let's order fresh items for you
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text(
+              "Let's order fresh items for you",
+              style: GoogleFonts.notoSerif(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 24),
+
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Divider(),
+          ),
+
+          const SizedBox(height: 24),
+
+          // categories -> horizontal listview
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Text(
+              "Fresh Items",
+              style: GoogleFonts.notoSerif(
+                //fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ),
+
+          // recent orders -> show last 3
+          Expanded(
+            child: Consumer<CartModel>(
+              // wrapped in consumer<>
+              builder: (context, value, child) {
+                return GridView.builder(
+                  padding: const EdgeInsets.all(12),
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: value.shopItems.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 1 / 1.2,
+                  ),
+                  itemBuilder: (context, index) {
+                    return GroceryItemTile(
+                      itemName: value.shopItems[index][0],
+                      itemPrice: value.shopItems[index][1],
+                      imagePath: value.shopItems[index][2],
+                      //imagePath: value.shopItems[index][3],
+                      color: value.shopItems[index][3],
+                      onPressed: () =>
+                          Provider.of<CartModel>(context, listen: false)
+                              .addItemToCart(index),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
